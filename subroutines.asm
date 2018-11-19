dd87587cfbb20d0fcc808045aa8eac9036f697e5
erl116 <erl116@skbl-422-014.ph.ic.ac.uk>
2018-11-19 15:57:11 +0000

    SAFE CODE: added some comments

--- subroutines.asm
+++ subroutines.asm
@@ -3,26 +3,26 @@
     global  keypad, compare, voice, unlock, release, delay, delayL, delayG, delayT, resetbreach, timeout, red, green, ledoff, redflash
     extern  clear_display
     extern  Mnewp, Mpin, Mincpin, M3inc, Mlock,Moldp, Munlock, Msuc, Mstar, Mbreach, Mspeak
-    extern  t3store, t5store
+    extern  t3store, t5store, lockstore, lockread
     extern  code1, code2, code3, code4, threetimes, fivetimes, checktostart, flag, wrongflag, delay_count, delay_countL, delay_countG, delay_countT, LED, voicebyte, flasher, col_store, row_store, lock
-    
     
     code 
     
     
 ;******************* keypad read ****************************			
-
+	;this routine reads the manual entry on the keypad by storing rows and columns of entry, with particular row and column corresponding to a particular key. 
+	;the routine uses port E, when code runs you are able to see the LEDS on this port light corresponding to the rows and columns pressed on the keypad. 
 keypad	
-outin   movlw	0x0f			;to read the rows 0x0f = 00001111
+outin   movlw	0x0f		    ;to read the rows 0x0f = 00001111
 	movwf	TRISE, ACCESS	    ;inputs 0-3, outputs 4-7
-	call	delay
+	call	delay		    ;delay lets the pin settle 
 rowstore
-	movff	PORTE, row_store	    ;stores row number 	
+	movff	PORTE, row_store    ;stores row number 	
 inout   movlw	0xf0		    ;to read th e columns 0xf0 = 1111000
 	movwf	TRISE, ACCESS	    ;outputs 0-3, inputs 4-7
 	call	delay 
 colstore		    
-	movff	PORTE, col_store	    ;column number is stored
+	movff	PORTE, col_store    ;column number is stored
 	call	delay 
 	
 read    movf	row_store, 0	    ;moving row store to Wreg
@@ -34,13 +34,17 @@ read    movf	row_store, 0	    ;moving row store to Wreg
 
 ;***************** comparison subroutine if correct numbers stored *************
 	
-compare clrf	flag		    ;this is useful for when the button is pressed half way through a keypad
+	;a subroutine which only lets the code store if the computer has correctly registered the rows and columns. during testing the computer may only store rows, which does not correspond to a number on the keypad. 
+	;the result of this subroutine is a high or low flag determining whether a correct number has been read which is allowed for the code 
+	
+compare	
+	clrf	flag		;sets the flag to 0     
 zero    movlw	0xbe
 	cpfseq 	checktostart 
 	goto	one
 	return
-one	movlw	0x77		    ;and the row and columns arent both stored properly. There are 
-	cpfseq 	checktostart	    ;
+one	movlw	0x77		    
+	cpfseq 	checktostart	    
 	goto	two 
 	return
 two	movlw	0xb7
@@ -73,11 +77,11 @@ eight	movlw	0xbd
 	return
 nine	movlw	0xdd
 	cpfseq 	checktostart
-	setf	flag
+	setf	flag			;if no correct number stored (0-9) the flag is set high. 
 	return
 	
 ;******************* release from command button ****************; 	
-	
+	;if the button is released, then the computer can begin to store the next button  
 release      ;checking if the buttons have been released 
 	call	keypad
 	movlw	0xff
@@ -86,14 +90,16 @@ release      ;checking if the buttons have been released
 	return 
 	
 ;*********************pin unlock  *******************
-unlock  clrf	lock
+unlock  setf	lock
+	movff   lock, PORTH
+	call	lockstore 
 	call	Munlock
 	call	green
 	call    delayG
 	call    ledoff
 	call    resetbreach
 	
-	return 
+	 return 
 	
 ;********************* voice unlock ****************************	
 voice   call    clear_display
@@ -104,7 +110,7 @@ voice   call    clear_display
 voicecheck
 	movff	PORTD, voicebyte
 	movlw	0x7f
-	cpfsgt	voicebyte    ;comapring voicebyte (input from portD) with 7F, if more than pin7 is lit and it skips a line
+	cpfsgt	voicebyte		;comapring voicebyte (input from portD) with 7F, if more than pin7 is lit and it skips a line
 	goto    incvoice
 	call    clear_display
 	call	unlock
@@ -112,14 +118,14 @@ voicecheck
 	
 incvoice
 	call    clear_display
+	call	Mincpin
 	call	red
 	call    delayG
 	call	ledoff 
-	call	Mincpin
 	decfsz  fivetimes
+	call    t5store
 	movlw   0x00
 	cpfsgt  fivetimes, 0
-	call    t5store
 	call    timeout
 	return 
 
@@ -139,23 +145,21 @@ resetbreach
 ;*********************** safe has been breached ********************	
 	
 timeout
-	call	clear_display
+	call	clear_display		    
 	call	Mbreach
-	call	redflash 
-	
-	call    resetbreach
+	call	redflash	    ;the time delay in redflash prevents the user from opening attempting to open the safe until it has finished
+	call    resetbreach	    ;resets the number of allowed attempts to 5 and 3 for voice and keypad entry respectively
 	call	clear_display 
 	return 
 	
-
 ;*********************** LED sequences *******************************
 green   
 	movlw   0x01
-	movwf   LED, ACCESS
+	movwf   LED, ACCESS	    ;turns green LED on, used for unlocking the safe
 	movff   LED, PORTD
 	return
 	
-red	movlw   0x02
+red	movlw   0x02		    ;turns red on when incorrect password
 	movwf   LED, ACCESS
 	movff   LED, PORTD
 	return
@@ -165,10 +169,10 @@ ledoff
 	movff   LED, PORTD
 	return 
 	
-redflash
+redflash			    ;makes red KED flash continuously for the time the safe is locked when breached 
 	movlw	0xff
 	movwf	flasher
-redflash1
+redflash1 
 	call	red
 	call	delayG 
 	call	ledoff
@@ -178,32 +182,32 @@ redflash1
 	return 
 	
 ;*********************** delay ****************************
-delay	movlw   0xff 
-	movwf   delay_count	
-delay1	decfsz	delay_count	; decrement until zero
+delay	movlw   0xff			;smallest delay
+	movwf   delay_count   	                                                 
+     delay1	decfsz	delay_count	; decrement until zero
 	bra	delay1
 	return
 	
-delayL  movlw	0xff	
+delayL  movlw	0xff			;second smallest delay
 	movwf   delay_countL
 delayL1	call    delay
 	decfsz  delay_countL
 	bra	delayL1
 	return 
 
-delayG  movlw	0xff	
+delayG  movlw	0xff 			;second largest delay 
 	movwf   delay_countG
 delayG1	call    delayL
 	decfsz  delay_countG
 	bra	delayG1
 	return 
 	
-delayT				   ;the longest delays require an argument for how many delay G you want 
+delayT					;the longest delay
 	movwf   delay_countT
 delayT1	call    delayG
 	decfsz  delay_countT
 	bra	delayT1
 	return 	
 	
-	
+;*******************************************************************	
 	end
